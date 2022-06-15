02c3bfb6fd23299dd956a04bfc85edcb97a9efb2
MarcoMedrano <marco.medrano.garay@gmail.com>
2022-06-15 16:17:26 -0400

    feat: Added couple of unittest

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -8,20 +8,34 @@ const {interface, bytecode } = require('../compile');
 let accounts;
 let inbox;
 
+const InitialMessage = 'Helllo nena';
 beforeEach(async () =>{
     // Get a list of all accounts
     accounts =  await web3.eth.getAccounts();
     
     // Use an account to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Helllo nena']})
+    .deploy({ data: bytecode, arguments: [InitialMessage]})
     .send({from: accounts[0], gas:'1000000'});
     
-    console.log('Accounts ', accounts);
-    console.log('Inbox ', inbox);
+    // console.log('Accounts ', accounts);
+    // console.log('Inbox ', inbox);
 });
 
 describe("Inbox", () => {
-    it('Will pass', ()=>{
+    it('Deploys a contract', ()=>{
+        assert.ok(inbox.options.address);
+    });
+    
+    it('Has default message', async ()=>{
+        const message = await inbox.methods.message().call(/*transaction options like gas*/);
+        assert.equal(message, InitialMessage);
+    });
+
+    it('Can change the message', async ()=>{
+        const res = await inbox.methods.setMessage('bye').send({from:accounts[0]});
+        console.log('Address ', res);
+        const message = await inbox.methods.message().call(/*transaction options like gas*/);
+        assert.equal(message, 'bye');
     });
 });
